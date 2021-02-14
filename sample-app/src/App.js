4bc9526929477ba14f6aba864622d250533fecb8
Omar Magdy <contact.parmag@gmail.com>
2021-02-14 21:19:19 +0200

    App.js:add:probs

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -4,7 +4,7 @@ import Product from "./components/product"
 class App extends Component {
   render = () => {
     return (
-        <Product />
+        <Product name="Labtob" price={50.1} in_stock={true} />
     );
   }
 }
--- sample-app/src/components/product.js
+++ sample-app/src/components/product.js
@@ -2,13 +2,14 @@ import {Component} from "react"
 
 class Product extends Component {
     render() {
-        return(
+        console.log(this.props)
+        /*return(
             <div className="product">
             <p>Name is: Labtop</p>
             <p>Price is: $50</p>
             <p>In Stock is: True</p>
             </div>
-        )
+        )*/
     }
 }
 
