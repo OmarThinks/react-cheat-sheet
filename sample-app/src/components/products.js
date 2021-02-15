2f199383519e1f6151e8f68f002bd04dedebbc90
Omar Magdy <contact.parmag@gmail.com>
2021-02-15 09:10:14 +0200

    products.js:add:conditional

--- sample-app/src/components/products.js
+++ sample-app/src/components/products.js
@@ -3,16 +3,20 @@ import {Component} from "react"
 const Products = (props) => {
     const products_list = props.products
     const to_render = products_list.map(product => {
-        return(
-            <div className="product" key={product.id}>
-                <p>Name is: {product.name}</p>
-                <p>Price is: ${product.price}</p>
-                <p>In Stock is: {product.in_stock.toString()}</p>
-                <hr/>
-            </div>
-        );}
+        if (product.in_stock) {
+            return(
+                <div className="product" key={product.id}>
+                    <p>Name is: {product.name}</p>
+                    <p>Price is: ${product.price}</p>
+                    <p>In Stock is: {product.in_stock.toString()}</p>
+                    <hr/>
+                </div>
+            );
+        } else {
+            return(null)
+        }
+    }
     )
-    console.log(products_list)
     return(
         <div className="products_list">
             { to_render }
