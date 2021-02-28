c612cc6f2063c71d4ef9c7539d5aca53f9343018
Omar Magdy <contact.parmag@gmail.com>
2021-02-28 14:23:27 +0200

    redux.md:add:create store

--- lessons/2)redux.md
+++ lessons/2)redux.md
@@ -37,6 +37,50 @@ npm install --save react-redux
 
 
 
+## 2) Creating a store:
+
+
+
+```Js
+import {Component} from 'react';
+import {createStore} from "redux";
+
+const initState = {
+  todos:[],
+  posts:[]
+}
+
+function myReducer(state = initState,action) {
+}
+
+const store = createStore(myReducer);
+
+class App extends Component {
+  render = () => {
+    return (
+      <div className="App">
+        Hi!
+      </div>
+    );
+  }
+}
+
+export default App;
+
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -1,23 +1,22 @@
 import {Component} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import About from "./components/about"
-import Contact from "./components/contact"
-import Home from "./components/home"
-import Navbar from "./components/navbar"
+import {createStore} from "redux";
+
+const initState = {
+  todos:[],
+  posts:[]
+}
+
+function myReducer(state = initState,action) {
+}
+
+const store = createStore(myReducer);
 
 class App extends Component {
   render = () => {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/about" component={About} />
-            </Switch>
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        Hi!
+      </div>
     );
   }
 }
